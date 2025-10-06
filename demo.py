73a680adb07f3ad04c5e5ef923d7447e08f99593
Uminelka <spehovdima8@gmail.com>
2025-10-06 15:50:13 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -4,10 +4,10 @@ seq = Seq("test", "ATCGTUAACG")
 print(f"Длина: {len(seq)}")
 print(f"Тип: {seq.alphabet()}")
 
-reader_one = FastaReader("small_example.fasta")
+reader_one = FastaReader("Fasta-project/examples/small_example.fasta")
 for sequence in reader_one:
     print(sequence)
 
-reader_two = FastaReader("big_example.fasta")
+reader_two = FastaReader("Fasta-project/examples/big_example.fasta.txt")
 for sequence in reader_two:
     print(sequence)
